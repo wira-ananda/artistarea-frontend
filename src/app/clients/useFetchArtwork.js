184ecc1e39e62be9004a5ec47ca24cae3bebdd37
Wiraa <wira007911@gmail.com>
2024-11-06 17:58:31 +0800

    feat(useMode): memperbaiki state management

--- src/app/clients/useFetchArtwork.js
+++ src/app/clients/useFetchArtwork.js
@@ -39,3 +39,4 @@ export const useFetchArtworkById = (id)=> {
   }, [id])
   return {artwork: artwork || [], loading}
 } 
+
--- src/app/clients/useMode.js
+++ src/app/clients/useMode.js
@@ -15,3 +15,16 @@ const useMode = () => {
 };
 
 export default useMode;
+
+// untuk deployment dilarang menggunakan localstorage
+
+// "use client"
+// import { useState } from "react";
+
+// const useMode = () => {
+//   const [isArtist, setIsArtist] = useState(false);
+//   const [isLogin, setIsLogin] = useState(true)
+//   return { isArtist, setIsArtist, isLogin, setIsLogin };
+// };
+
+// export default useMode;
